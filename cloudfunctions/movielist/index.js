8e9fcc4a2eb4391c68f4a74764f598cb650c2ac5
buyuanlee <buyuanlee@gmail.com>
2019-05-14 20:10:51 +0800

    调用豆瓣电影api接口

--- cloudfunctions/movielist/index.js
+++ cloudfunctions/movielist/index.js
@@ -2,17 +2,19 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init()
-//引入request-promise
+
 var rp = require('request-promise');
 
 // 云函数入口函数
-exports.main = async(event, context) => {
+exports.main = async (event, context) => {
 
-  return rp(`https://api.douban.com/v2/movie/in_theaters?start=${event.start}&count=${event.count}`)
-    .then(function(res) {
-      return res
+
+  return rp(`http://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=${event.start}&count=${event.count}`)
+    .then(function (res) {
+      console.log(res);
+      return res;
     })
-    .catch(function(err) {
-      console.err(err)
+    .catch(function (err) {
+      console.err(err);
     });
 }
