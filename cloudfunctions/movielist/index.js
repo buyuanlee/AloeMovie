3d65d088a4076657563081d872470e18033573e9
buyuanlee <buyuanlee@gmail.com>
2019-05-14 17:00:25 +0800

    引入豆瓣api

--- cloudfunctions/movielist/index.js
+++ cloudfunctions/movielist/index.js
@@ -8,11 +8,11 @@ var rp = require('request-promise');
 // 云函数入口函数
 exports.main = async(event, context) => {
 
-  return rp('http://www.google.com')
-    .then(function(htmlString) {
-      // Process html...
+  return rp(`https://api.douban.com/v2/movie/in_theaters?start=${event.start}&count=${event.count}`)
+    .then(function(res) {
+      return res
     })
     .catch(function(err) {
-      // Crawling failed...
+      console.err(err)
     });
 }
