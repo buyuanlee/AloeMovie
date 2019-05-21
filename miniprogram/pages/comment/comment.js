ae1537b33f870d72dd1f6bd5e1e6945c70e4773a
buyuanlee <buyuanlee@gamil.com>
2019-05-21 15:36:46 +0800

    详情页

--- miniprogram/pages/comment/comment.js
+++ miniprogram/pages/comment/comment.js
@@ -1,43 +1,29 @@
-// pages/movie/movie.js
+// miniprogram/pages/comment/comment.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    movieList: []
-  },
-  getMovieList: function() {
-    wx.showLoading({
-      title: '加载中',
-    })
-    wx.cloud.callFunction({
-      name: 'movielist',
-      data: {
-        start: this.data.movieList.length,
-        count: 10
-      }
-    }).then(res => {
-      // console.log(res);
-      this.setData({
-        movieList: this.data.movieList.concat(JSON.parse(res.result).subjects)
-      });
-      wx.hideLoading();
-    }).catch(err => {
-      console.error(err);
-      wx.hideLoading();
-    });
-  },
-  gotoComment: function(event) {
-    wx.navigateTo({
-      url: `../comment/comment?movieid=${event.target.dataset.movieid}`,
-    });
+    details:{}
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    this.getMovieList()
+    console.log(options)
+    wx.cloud.callFunction({
+      name: "getDetails",
+      data: {
+        movieid: options.movieid
+      }
+    }).then(res => {
+      console.log(res)
+      this.setData({
+        details:JSON.parse(res.result)
+      })
+    }).catch(err => {
+      console.error(err)
+    })
   },
 
   /**
@@ -79,7 +65,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-    this.getMovieList();
+
   },
 
   /**
